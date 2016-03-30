c7bd6d72714dc48dcfbd5e22fd766b895649db21
Ivan Sergeyenko <svintus@gmail.com>
2016-03-29 20:30:07 -0400

    grid view controller

---
+++ ImgNest/ImageCollectionViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ImageCollectionViewController.swift
+//  ImgNest
+//
+//  Created by Ivan Sergeyenko on 2016-03-29.
+//  Copyright © 2016 Bruce and Willis. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewController: UICollectionViewController {
+
+  var images: Array<String>!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.images = [
+      "img1",
+      "img2",
+      "img3",
+      "img4",
+      "img5",
+    ]
+  }
+  
+  override func collectionView(collectionView: UICollectionView,
+      numberOfItemsInSection section: Int) -> Int {
+    return self.images.count
+  }
+
+  override func collectionView(collectionView: UICollectionView,
+      cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+
+   let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell",
+       forIndexPath: indexPath)
+   let imageView = cell.viewWithTag(42) as! UIImageView
+   let image = UIImage(named: self.images[indexPath.row])
+   print("image: \(image)")
+   imageView.image = UIImage(named: self.images[indexPath.row])
+   
+   return cell
+  }
+}
